aca7bc2d638abb39245dee73b365be307a313df3
Qstrich <128658949+Qstrich@users.noreply.github.com>
2024-06-06 18:24:04 +0000

    c

--- Item.java
+++ Item.java
@@ -1,4 +1,18 @@
 
 public abstract class Item {
+    private String name;
+    private double price;
+    private int stock;
+    private String s;
+    private int id; 
+    
+    public Item(String name, double price, int stock, String s, int id) {
+
+    }
+    public abstract boolean useItem();
+    public boolean enoughStock(int amt) {
+        
+    }
+
 
 }
---
+++ ItemManager.java
@@ -0,0 +1,40 @@
+public class ItemManager {
+    private int maxItems; 
+    private int currentItemNum;
+    private Item[] items;
+
+    public ItemManager(int maxItems) {
+
+    }
+    public void sortItemAscendingPriceAlpha() {
+
+    }
+    public void sortItemDescendingPriceAlpha() {
+
+    }
+    public void sortItemName() {
+
+    }
+    public void listItems() {
+
+    }
+    public void listItemsBetweenPrice(double l, double r) {
+
+    }
+    public Item searchItem(int id) {
+
+    }
+    public void deleteItem(Item i) {
+
+    }
+    public boolean addItem(int, String, double, int, String, int, String) {
+
+    }
+    public boolean loadFromFile(String fileName) {
+
+    }
+    public boolean saveToFile(String fileName) {
+
+    }
+
+}
