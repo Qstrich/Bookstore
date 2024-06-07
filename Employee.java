cbb718b4bbf1bc0424578b9d06894b7a7e876a49
bookstoreproject1234 <bookstoreproject1234@gmail.com>
2024-06-07 17:24:25 +0000

    employee

--- Employee.java
+++ Employee.java
@@ -1,15 +1,12 @@
 
 public class Employee extends Account {
-    
- 
+
     public static double DISCOUNT = 8;
     public static final String EMPLOYEE_KEY = "BusinessAYJ";
 
     public Employee(String name, String password){
         super(name, password);
     }
-
-  
 
     public double getDiscount(){
         return DISCOUNT;
